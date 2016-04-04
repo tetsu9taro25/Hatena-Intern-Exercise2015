26a154733911ba3e543244734880fd27867db57a
Tetsutaro Endo <tetsu9taro25@gmail.com>
2016-04-05 03:41:08 +0900

    Task JS-1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
 // 課題 JS-1: 関数 `parseLTSVLog` を記述してください
+var logStr =
+  "path:/\tepoch:200000\n" +
+  "path:/help\tepoch:300000\n" +
+  "path:/\tepoch:250000\n";
+
+function parseLTSVLog (rowData){
+
+  var parse = [];
+  colLog = rowData.split(/\n/);//各行で切り分ける
+
+  for (i=0; i<colLog.length-1; i++){
+    logElement = colLog[i].split(/\t/);//tabで切り分ける
+    var obj = {};
+
+    for (j=0; j<logElement.length; j++){
+      keyVal = logElement[j].split(':');
+
+      if (keyVal[0]==='epoch'){
+        obj[keyVal[0]] = Number(keyVal[1]);
+      }else{
+        obj[keyVal[0]] = keyVal[1];
+      }
+    }
+    parse[i] = obj;
+  }
+ return parse;
+}
+
+parseLTSVLog(logStr);
 
 // 課題 JS-2: 関数 `createLogTable` を記述してください
