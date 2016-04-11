82d5cbaca4127c25c6afebc5895d1ca1781b9b82
Tetsutaro Endo <tetsu9taro25@gmail.com>
2016-04-12 07:30:39 +0900

    add Perl-2

--- perl/lib/Parser.pm
+++ perl/lib/Parser.pm
@@ -8,6 +8,21 @@ sub new {
 }
 
 sub parse {
+  my $class = @_;
+  my $self = shift;
+  open my $fh, '<', $self->{filename} or die $!;
+  my @parsed_lines;
+  my @temp;
+  my $i = 0;
+  while(my $lines = <$fh>){
+    $lines =~ s/:\/\//Z\/\//;
+    $lines =~ s/\n//;
+    my %hash = split(/[:\t]/, $lines);
+    $hash{referer} =~ s/Z\/\//:\/\//;
+    $parsed_lines[$i] = bless \%hash, 'Log';
+    $i++;
+  }
+  return \@parsed_lines;
 }
 
 1;
